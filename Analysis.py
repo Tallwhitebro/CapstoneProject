3760819be451975fa2feee00292ff58ff0973678
Tallwhitebro <ollersjp@mcmaster.ca>
2020-04-29 04:27:44 -0400

    Code Cleanup and Comments

--- Analysis.py
+++ Analysis.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Initial Data Analysis - Capstone Project
-
-# In[49]:
-
-
 # Imports
 import os
 import csv
@@ -16,21 +8,14 @@ import pickle
 import sys
 import math
 
-
-# In[50]:
-
-
+# Save object into compressed pickle file
 def save_as_pkl(object, path):
 	pickle.dump(object, open(path, "wb"))
 
+# Load from pickle file
 def load_pkl(path):
 	obj = pickle.load(open(path, "rb"))
 	return obj
-
-
-# ### Preprocessing and loading the data
-
-# In[51]:
 
 
 # Processing the .csvs into dataframes and saving them as pickles for easier
@@ -45,10 +30,6 @@ for file in files:
     print(df.shape)
     save_as_pkl(df, 'pickles/'+fName+'.pkl')
 
-
-# In[52]:
-
-
 ## Loading preprocessed dataframes
 pklFiles = [f for f in os.listdir("pickles/") if f.split('1')[0] == 'file']
 for file in pklFiles:
@@ -62,16 +43,12 @@ with open('data/DistanceCalculated.csv', newline='') as f:
 
 years = [10,11,12,13,14,15,16,17]
 for y in years:
-    # YEAR:
     year = y
     fname = "file"+str(year)+".pkl"
     df = load_pkl("pickles/"+fname)
 
 
-    # ### Starting to analyze the data
-
-    # In[54]:
-
+    ### Starting to analyze the data
 
     # First 30 columns:
     firstPart = df.iloc[:,0:30]
@@ -79,41 +56,19 @@ for y in years:
 
     # Columns 30 to 65: (top 6 course marks)
     top6CourseMarks = df.iloc[:,30:66]
-    # print(top6CourseMarks.columns.values)
-
-    # cols = []
-    # for column in top6CourseMarks[top6CourseMarks.columns[2::3]]:
-    #     cols.append(df[column])
-    # hstack = pd.concat([x for x in cols], axis=1)
-    # hstack.fillna(0, inplace=True)
-    # hstack['Sum'] = hstack.mean(axis=1)
-    # print(hstack)
 
     # Columns 66 to 71:
     middle = df.iloc[:,66:72]
-    # print(middle["WAVERG1"], middle["WAVERG2"])
-    # print(middle.columns.values)
 
     # Columns 72 to 391:
     choices = df.iloc[:, 72:]
-    # print(choices.columns.values)
-
-
-    # In[55]:
-
-
-    # print(top6CourseMarks.head())
-    # print(top6CourseMarks.columns)
-
-
-    # In[56]:
-
 
     courseCodeCols = ['SECORCOD' + str(i+1) for i in range(12)]
-    maxScore = ['SECORC' + str(i+1) for i in range(12)] #I'm assuming that's what this column is? (useless)
+    maxScore = ['SECORC' + str(i+1) for i in range(12)]
     studentScoreCols = ['SECORM' + str(i+1) for i in range(12)]
     goalCourses = ['MHF4U', 'MCV4U', 'ENG4U', 'SCH4U', 'SPH4U']
 
+    # Target courses:
     # Advanced functions: MHF4U
     # Calculus: MCV4U
     # English: ENG4U
@@ -121,42 +76,35 @@ for y in years:
     # Physics: SPH4U
     # Next highest mark, anything
 
-
-    # In[57]:
-
-
+    # Calculates student's averages given the 5 required courses for entry
     def averageFinder(allStudentMarks):
         studentAverages = []
         for index, row in allStudentMarks.iterrows(): 
             try:
+                # The courses the student took
                 studentCourseCodes = [row[courseCodeCol] for courseCodeCol in courseCodeCols]
-                studentGrades = [row[studentScoreCol] for studentScoreCol in studentScoreCols]
                 studentCourseCodes = [val for val in studentCourseCodes if type(val) == str]
+
+                # The grades the student received in each course
+                studentGrades = [row[studentScoreCol] for studentScoreCol in studentScoreCols]
                 studentGrades = [val for val in studentGrades if str(val) != 'nan']
 
+                # Required course
                 necessaryCourses = [int(studentGrades[i]) for i in range(len(studentGrades)) if studentCourseCodes[i] in goalCourses]
+                # Remaining course(s)
                 remainder = [int(studentGrades[i]) for i in range(len(studentGrades)) if studentCourseCodes[i] not in goalCourses]
 
-                # Some students don't seem to have 6 courses.
-                # Set their average to -1 in this case.
-                # if len(necessaryCourses) != 5:
-                #     studentAverages.append(np.nan)
-                #     print
-
+                # The number of courses the student applied with.
+                # Some student's data seems to be inaccurate, they show
+                # less than the 6 required courses.
                 length = len(necessaryCourses) + min(len(remainder),1)
-
                 average = (max(remainder) + sum(necessaryCourses))/length
                 studentAverages.append(average)
+
             except ValueError:
-    #             print("Student with incorrect number of courses applied.")
                 studentAverages.append(np.nan)
+
         return studentAverages
-        
-    # print(averageFinder(top6CourseMarks))
-
-
-    # In[58]:
-
 
     # Finding the student's choice preference for the university:
     # This data is found in the 'choices' dataframe.
@@ -164,24 +112,21 @@ for y in years:
     programChoiceCols = ['DPRO1' + str(i+1) for i in range(20)]
     goalUni = '196'
 
-
-    # In[59]:
-
-
+    # Determine which preference value the student 
+    # gave to our target university
     def uniChoiceFinder(studentUniChoices):
         studentAverages = []
         for index, row in studentUniChoices.iterrows():
             # Filtering student's preference for our university and our specific program.
             studentCourseCodes = [i+1 for i in range(20) if (row['DUNI'+str(i+1)] == 196
-                                                                                    and (row['DPRO'+str(i+1)] == 'SIA' 
-                                                                                    or row['DPRO'+str(i+1)] == 'SI'))]
+                                                                    and (row['DPRO'+str(i+1)] == 'SIA' 
+                                                                    or row['DPRO'+str(i+1)] == 'SI'))]
             studentAverages.append(studentCourseCodes[0])
+
         return studentAverages
 
-
-    # In[60]:
-
-
+    # Determine which student's received an offer
+    # from the target university and accepted the offer.
     def acceptedOurUni(firstPart):
         acceptedArray = []
         for index, row in firstPart.iterrows():
@@ -190,32 +135,21 @@ for y in years:
             acceptedArray.append(studentAccepted)
         return acceptedArray
 
-
-    # # Determining grade cutoff
-
-    # ### Seperating relevant data
-    # - Splitting data into students who accept and offer from mac and those who don't.
-    # - Isolating 8 initial columns of interest for analysis
-
-    # In[ ]:
-
-
-    ## Starting columns of interest:
-    # RESPROV - Province of Residence
-    # RESCNTY - County of Residence
+    # Columns of interest:
+    # GEND - The student's gender
+    # SCHOOL - The student's highschool ID
     # ZIP3 - Residence Postal Code (First 3 Digits)
     # CONFUNI - Confirmed University (OurUni='196')
-    # CONFPR - Confirmed Program (OurProg='SI', OurProg_coop='SIA')
     # CONFCHOIC - OUAC Confirmed Choice Preference
-    # WAVERG1 - Weighted Average (best 6 OAC / Senior Level current year finals)
     # WAVERG2 - Weighted Average (best 6 OAC / Senior Level all year finals)
+    # Note, WAVERG2 differs from the calculated average given the 5 required courses.
+
     ## First 6 columns are in first half, last 2 are in "Middle" dataframe.
 
-    # Removed ZIP3 and CONFPR for now as they are not float values
     COIsFirstHalf = ["GEND", "SCHOOL", "ZIP3", "CONFUNI", "CONFCHOIC"]
     COIsSecondHalf = ["WAVERG2"]
 
-    # Finding the student's average from the formula given by Dr. Franek.
+    # Finding the student's average using the 5 required courses
     averages = averageFinder(top6CourseMarks)
     averagesDf = pd.DataFrame(averages, columns=['AVG'])
 
@@ -234,14 +168,17 @@ for y in years:
     columnsOfInterest = pd.concat([columnsOfInterest] + [middle[x] for x in COIsSecondHalf] + 
                                   [averagesDf] + [preferencesDf] + [acceptedDf],axis=1)
 
+    # Determining the list of zip codes
     zipCodes = []
-
     zipData = columnsOfInterest[columnsOfInterest["ZIP3"].notna()]["ZIP3"]
     zipSet = set(list(zipData.to_numpy()))
     for zipCode in zipSet:
         if zipCode not in zipCodes:
             zipCodes.append(zipCode)
 
+    # For each zip code present, determine the 
+    # corresponding distance from the target
+    # university.
     distanceDict = {}
     for distance in distances:
         try:
@@ -249,79 +186,35 @@ for y in years:
         except ValueError:
             pass
 
-    columnsOfInterest['DIST'] = columnsOfInterest.apply(lambda row: distanceDict[row.ZIP3] if row.ZIP3 in distanceDict else np.nan, axis = 1)
+    # For each row, add the corresponding distance from 
+    # the unversity to a column labeled 'DIST'
+    columnsOfInterest['DIST'] = columnsOfInterest.apply(lambda row: distanceDict[row.ZIP3]
+            if row.ZIP3 in distanceDict else np.nan, axis = 1)
 
     copy = columnsOfInterest.copy()
 
-    mcmasterVector = copy["CONFUNI"] == 196
-    notmcmasterVector = copy["CONFUNI"] != 196
+    targetUni = copy["CONFUNI"] == 196
+    notTargetUni = copy["CONFUNI"] != 196
     allStudents = copy
 
+    # Students that received and accepted an offer
+    # from the target university:
+    acceptedTargetUni = copy[targetUni]
 
-    # ### Some min/max/average output for both student types:
-
-    # In[ ]:
-
-
-    # Students that received and accepted a McMaster offer:
-    acceptedMcMaster = copy[mcmasterVector]
-    # print(acceptedMcMaster)
-    # print(acceptedMcMaster[(acceptedMcMaster["WAVERG2"] > 0) & (acceptedMcMaster["CONFPR"] == "SIA")].min())
-    # print("Students who accepted an offer from McMaster:")
-    # print("Mean values for each column:")
-    # print(acceptedMcMaster[(acceptedMcMaster["AVG"] > 0) & (acceptedMcMaster["CONFPR"] == "SIA")].mean())
-    # print("\nMax values for each column:")
-    # print(acceptedMcMaster[(acceptedMcMaster["AVG"] > 0) & (acceptedMcMaster["CONFPR"] == "SIA")].max())
-    # print("\nLowest 5 weighted average:")
-    # print(acceptedMcMaster[(acceptedMcMaster["AVG"] > 0) & (acceptedMcMaster["CONFPR"] == "SI")].nsmallest(20,"WAVERG2"))
-    # print("\nShape of acceptedMcMaster df (number of students that accepted):")
-    # print(acceptedMcMaster.shape)
-
-    # Calculating the student with the lowest average who accepted our university.
+    # Calculating the student with the lowest average who accepted the target university.
     # This is used to calculate the cutoff point.
-    smallest = acceptedMcMaster[(acceptedMcMaster["AVG"] > 0) & (acceptedMcMaster["ACCEPTED"] == 1)].nsmallest(1,"AVG").iloc[0]['AVG']
+    smallest = acceptedTargetUni[(acceptedTargetUni["AVG"] > 0) & (acceptedTargetUni["ACCEPTED"] == 1)].nsmallest(1,"AVG").iloc[0]['AVG']
     # From this, generating the list of all students who received an offer.
-    receivedOffer = acceptedMcMaster[(acceptedMcMaster["AVG"] >= smallest)]
+    receivedOffer = acceptedTargetUni[(acceptedTargetUni["AVG"] >= smallest)]
 
-    # # Students that didn't accept a mcmaster offer:
-    # print('\n\n\n\n')
-    notMcMaster = copy[notmcmasterVector]
-    # # print(notMcMaster)
-    # print("Min average of students who didn't accept mac offer")
-    # print(notMcMaster[notMcMaster["AVG"] > 0].min())
-    # print("\nMax average of students who didn't accept mac offer")
-    # print(notMcMaster[notMcMaster["AVG"] > 0].max())
-    # print("\nAverage average of students who didn't accept mac offer")
-    # print(notMcMaster[notMcMaster["AVG"] > 0].mean())
+    # # Students that didn't accept a received offer
+    didntAccept = copy[notTargetUni]
 
-
-    # # In[ ]:
-
-
-    # # print(acceptedMcMaster.shape)
-    # # print(notMcMaster.shape)
-    # print('McMaster students sample:')
-    # print(acceptedMcMaster.head(10))
-    # print('\n\n')
-    # print('All Students sample:')
-    # print(allStudents.head(20))
-    # print(notMcMaster.head(50))
-
-
-    # ## Exporting the dataframes to CSV
-
-    # In[ ]:
-
+    # Exporting the dataframes to CSV
     allStudents.dropna(axis=0, how='any', thresh=None, subset=None, inplace=True) #dropping all rows that contain an empty value
     allStudents.to_csv('cleaned_data/allStudents/allStudents_'+str(year)+'.csv',index=False)
-    acceptedMcMaster.to_csv('cleaned_data/acceptedOurUni/acceptedOurUni_'+str(year)+'.csv',index=False)
-    notMcMaster.to_csv('cleaned_data/didntAccept/didntAccept_'+str(year)+'.csv',index=False)
+    acceptedTargetUni.to_csv('cleaned_data/acceptedOurUni/acceptedOurUni_'+str(year)+'.csv',index=False)
+    didntAccept.to_csv('cleaned_data/didntAccept/didntAccept_'+str(year)+'.csv',index=False)
     receivedOffer.to_csv('cleaned_data/receivedOffer/receivedOffer_'+str(year)+'.csv',index=False)
     print(year)
-
-
-    # In[ ]:
-
-
-
 
